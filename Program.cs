b31dfb8555ec8c658bbe53232958dcb105e642b0
Timur <sirakanyantimur1@gmail.com>
2024-01-31 13:35:54 +0400

    task3

--- Program.cs
+++ Program.cs
@@ -32,29 +32,64 @@
 // Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
 // Даны два неотрицательных числа m и n.
 
-int Akkermann (int m,int n)
+// int Akkermann (int m,int n)
 
-{   int result = 0;
+// {   int result = 0;
 
-    if(m == 0)
-{
-    result = n+1;
-}
+//     if(m == 0)
+// {
+//     result = n+1;
+// }
 
- if(m>0 && n==0)
+//  if(m>0 && n==0)
+//  {
+//     result = Akkermann(m-1,1);
+//  }
+//   if(m>0 && n>0)
+//   {
+//     result = Akkermann(m-1,Akkermann(m,n-1));
+//   } 
+
+//   return result;
+// }
+// Random Chukcha = new Random();
+// int m = Chukcha.Next(1,3);
+// int n = Chukcha.Next(1,4);
+// System.Console.WriteLine(m+ " "+ n);
+// int resultAkkermannchik = Akkermann(m,n);
+// System.Console.WriteLine(resultAkkermannchik);
+
+
+// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+Random rnd=new Random ();
+int [] rndArray = new int [rnd.Next(1,5)];
+ 
+ 
+ for(int i = 0;i < rndArray.Length;i++)
  {
-    result = Akkermann(m-1,1);
+   rndArray[i]= rnd.Next(1,31); 
  }
-  if(m>0 && n>0)
+  
+  for(int i = 0;i < rndArray.Length;i++)
   {
-    result = Akkermann(m-1,Akkermann(m,n-1));
-  } 
+    System.Console.Write(rndArray[i] + " " );
+  }
 
-  return result;
-}
-Random Chukcha = new Random();
-int m = Chukcha.Next(1,3);
-int n = Chukcha.Next(1,4);
-System.Console.WriteLine(m+ " "+ n);
-int resultAkkermannchik = Akkermann(m,n);
-System.Console.WriteLine(resultAkkermannchik);
+  void vsArrayPrint(int [] array,int i )
+  { 
+    if(i<=0)
+    {
+      System.Console.Write( array[i]+ " " );
+     
+     return; 
+    }
+    System.Console.Write(array [i] + " ");
+    vsArrayPrint(array,i-1);
+  }
+  int index = rndArray.Length - 1;
+
+  System.Console.WriteLine();
+
+  vsArrayPrint(rndArray,index);
